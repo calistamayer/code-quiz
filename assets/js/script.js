625b679bc9c58907c22e0e8836ce3cae86b0fab8
Calista Mayer <calista.a.mayer@gmail.com>
2020-11-10 17:31:56 -0800

    populate timer and quiz divs on start button click

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,12 @@
 // get html elements
 var startButtonEl = document.getElementById("start-button");
 var quizContainerEl = document.getElementById("quiz-container");
+var timerDivEl = document.getElementById("timer-div");
 
-// create handler function for start button click
+// start button click handler and listener
 var startButtonHandler = function () {
-    quizContainerEl.innerHTML = "";
+    quizContainerEl.innerHTML = "What does CSS stand for?";
+    timerDivEl.innerHTML = "You have 60 seconds left";
 }
 
-// event listener for start button click
 startButtonEl.addEventListener("click", startButtonHandler);
