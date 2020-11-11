d4f55f9897f873057b6a686a4e291e859df43e2e
Calista Mayer <calista.a.mayer@gmail.com>
2020-11-10 17:26:04 -0800

    clear quiz container with start button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,11 @@
+// get html elements
 var startButtonEl = document.getElementById("start-button");
+var quizContainerEl = document.getElementById("quiz-container");
+
+// create handler function for start button click
+var startButtonHandler = function () {
+    quizContainerEl.innerHTML = "";
+}
+
+// event listener for start button click
+startButtonEl.addEventListener("click", startButtonHandler);
